a5734c9c7eee4af15c50636c90a4fa0a215a8431
Brian Blain <bblain7@gmail.com>
2017-06-17 15:39:18 -0700

    Moved the ansible script and cleanup
    The ansible installation was moved directly into the install-software.sh
    so that only one file has to be run. There was also some cleanup needed
    and a patch because some of the Ansible roles are broken.

--- README.md
+++ README.md
@@ -16,16 +16,35 @@ This repository installs the following software on a baseline Debian Linux OS.
 
 # Usage
 
-This project has a couple scripts to install software on a new development machine.
-
-First run:
-
-```
-sudo sh scripts/ansible.sh
-```
-
-Once Ansible is installed then all the software can be installed via Ansible configuration.
+The installation script installs Ansible then all the software can be installed via Ansible configuration.
 
 ```
 sudo sh install-software.sh
+```
+
+For a completely automated execution of the installation script, configure SSH keys for github.com.
+
+https://help.github.com/articles/connecting-to-github-with-ssh/
+
+
+
+# FIXME
+
+I'm not sure why but the FGtatsuro.virtualbox fails the first time and has to be run twice. I think because the apt repo is not updating correctly. There is also a patch that I'm adding to the script so that it works with Ubuntu Zesty Zappas.
+
+```
+TASK [FGtatsuro.virtualbox : Run tasks on current environment] **************************************
+included: /home/brian/git/home/ansible/roles/FGtatsuro.virtualbox/tasks/Debian.yml for localhost
+
+TASK [FGtatsuro.virtualbox : Add key for the repository of VirtualBox] ******************************
+skipping: [localhost]
+
+TASK [FGtatsuro.virtualbox : Add key for the repository ov VirtualBox(latest distribution)] *********
+changed: [localhost]
+
+TASK [FGtatsuro.virtualbox : Add the repository of VirtualBox] **************************************
+ok: [localhost]
+
+TASK [FGtatsuro.virtualbox : Install VirtualBox] ****************************************************
+fatal: [localhost]: FAILED! => {"changed": false, "failed": true, "msg": "No package matching 'virtualbox-5.1' is available"}
 ```
--- install-software.sh
+++ install-software.sh
@@ -1,5 +1,74 @@
 #!/bin/bash
 
+
+#if [ $(/usr/bin/id -u) -ne 0 ]; then
+#  echo "Not running as root"
+#  exit
+#fi
+
+DIR=$( dirname "$( readlink -f "$0" )" )
+cd ${DIR}
+
+software_dir="${HOME}/software"
+
+
+#
+# Install Ansible
+# -------------------------------------------------------------------
+
+getAnsibleVersion() {
+  echo "$( ansible --version | awk '/^ansible/ {print $2}' )"
+}
+
+ansible_version="$( getAnsibleVersion )"
+if [ -z ${ansible_version} ]; then
+  sudo apt-get install software-properties-common -y
+
+  # Have to manually set this to the last distro until zesty is released by Ansible
+  #apt-add-repository ppa:ansible/ansible -y
+  sudo apt-add-repository 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu yakkety main'
+
+  sudo apt-get update
+  sudo apt-get install ansible -y
+
+  ansible_version="$( getAnsibleVersion )"
+  echo "${ansible_version}"
+
+  if [ ! -z ${ansible_version} ]; then
+    echo "Ansible ${ansible_version} successfully installed."
+  else
+    echo "Ansible was not installed."
+    exit 1;
+  fi
+else
+  echo "Ansible ${ansible_version} already installed."
+fi
+
+ansible --version || {
+  echo "Failed to install Ansible.";
+  exit 1;
+}
+
+
+#
+# Apply patches
+# -------------------------------------------------------------------
+
+# Copy the vars/main.yml for FGtatsuro.virtualbox so that it works
+# in Ubuntu Zesty Zappas
+vars_dir="${DIR}/ansible/roles/FGtatsuro.virtualbox/vars"
+sudo mkdir -p "${vars_dir}"
+sudo cp -f "${DIR}/patches/FGtatsuro.virtualbox/main.yml" "${vars_dir}"
+
+# Also to fix FGtatsuro, update the apt repos
+sudo apt update
+
+
+#
+# Execute Ansible Playbooks
+# -------------------------------------------------------------------
+
+
 cd ansible
 ansible-galaxy install -r requirements.yml
 ansible-playbook playbooks/developer.yml
